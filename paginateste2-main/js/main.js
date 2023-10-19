d0345e66eb50a7b2a85bb3b82f669bcd27a2a0eb
annaviana06 <139992353+annaviana06@users.noreply.github.com>
2023-10-18 22:53:34 -0300

    Add files via upload

---
+++ paginateste2-main/js/main.js
@@ -0,0 +1,20 @@
+// Função para mostrar ou ocultar o botão "Voltar ao Topo" conforme a rolagem
+window.onscroll = function() {
+    scrollFunction();
+  };
+  
+  function scrollFunction() {
+    var btnScrollToTop = document.getElementById("btnScrollToTop");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      btnScrollToTop.style.display = "block";
+    } else {
+      btnScrollToTop.style.display = "none";
+    }
+  }
+  
+  // Função para rolar suavemente de volta ao topo quando o botão é clicado
+  function scrollToTop() {
+    document.body.scrollTop = 0; // Para navegadores da web
+    document.documentElement.scrollTop = 0; // Para IE, Firefox, Chrome, Opera, Safari
+  }
+  
